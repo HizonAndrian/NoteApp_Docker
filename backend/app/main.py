2260cc65df51f2a1f96818386ce4d08759d8c71d
HizonAndrian <markandrianhizon@gmail.com>
2025-09-28 21:36:44 +0800

    Published get method.

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
+from bson import ObjectId
 
 # Pydantic model for note item.
 class note_item(BaseModel):
@@ -28,14 +29,37 @@ note_db = client.get_database()
 
 @app.post("/note", response_model=note_item, status_code=201)
 async def create_note(note: note_item):
+    # Insert the note into MongoDB.
     result = await note_db.notes.insert_one(note.model_dump())
     created_note = note.model_dump()
     created_note["id"] = str(result.inserted_id)
     return created_note
 
-@app.get("/note")
-def get_note():
-    return {"message": "GET: Hello World"}
+@app.get("/notes")
+async def get_note():
+    # Create a list to hold notes.
+    notes_collected = []
+    # Retrieve notes from MongoDB.
+    notes_reference = note_db.notes.find()
+    async for note in notes_reference:
+        # Convert ObjectId to string.
+        note["id"] = str(note["_id"])
+        # Remove the _id field from the note. It can cause error.
+        note.pop("_id")
+        notes_collected.append(note)
+    return notes_collected
+
+@app.get("/note/{id}")
+async def get_note_id(id: str):
+    note = await note_db.notes.find_one({"_id": ObjectId(id)})
+    if note is not None:
+        # Convert ObjectId to string.
+        note["id"] = str(note["_id"])
+        note.pop("_id")
+        return note
+    return {"error": "Note not found"}
+
+
 
 @app.put("/note")
 def update_note():
@@ -45,15 +69,4 @@ def update_note():
 @app.delete("/note")
 def delete_note():
     return {"message": "DELETE: Hello World"}
-
-
-
-
-@app.get("/test-mongo")
-def test_mongo():
-    try:
-        collections = note_db.list_collection_names()
-        return {"status": "connected", "collections": collections}
-    except Exception as e:
-        return {"status": "error", "message": str(e)}
 
